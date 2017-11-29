3c334a94d5b607754c51b41428d0891b092a30d3
Leo N <32232486+lnugraha@users.noreply.github.com>
2017-11-29 14:29:07 +0800

    Add files via upload

---
+++ trf_keras.py
@@ -0,0 +1,9 @@
+from keras.applications.xception import Xception
+from keras.applications.vgg16 import VGG16
+from keras.applications.vgg19 import VGG19
+from keras.applications.resnet50 import ResNet50
+from keras.applications.inception_v3 import InceptionV3
+from keras.applications.inception_resnet_v2 import InceptionResNetV2
+from keras.applications.mobilenet import MobileNet
+
+model = VGG16(weights='imagenet', include_top=True)
